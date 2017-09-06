287cf897059d164682d54c407bfdc31dffef78c7
Bianca Power <drbiancampower@gmail.com>
2017-09-06 14:36:14 +1000

    added projects subpages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
 
   get 'projects', to: 'pages#projects'
 
+  get 'projects/css-zen-garden', to: 'projects#css-zen-garden'
+
+  get 'projects/eb-games-redesign', to: 'projects#eb-games-redesign'
+
+  get 'projects/amaysim-clone', to: 'projects#amaysim-clone'
+
   get 'about', to: 'pages#about'
 
 end
