171b74ab6c1c6bca433cd7948e4d57d621039b79
Bianca Power <drbiancampower@gmail.com>
2017-12-02 17:54:24 +1100

    cleaned up old code, removed not needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
   post 'submission/submit'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: 'pages#homepage'
 
   get 'projects', to: 'pages#projects'
 
-  get 'projects/css-zen-garden', to: 'projects#css-zen-garden'
-
-  get 'projects/eb-games-redesign', to: 'projects#eb-games-redesign'
-
-  get 'projects/amaysim-clone', to: 'projects#amaysim-clone'
-
   get 'about', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
 
 end
